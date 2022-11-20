1321ec72d69fae55de29a91f081c93d5a6565929
Marek Szulc <235991@edu.p.lodz.pl>
2022-11-20 15:58:24 +0100

    fix open close functions

--- public/js/admin_dashboard.js
+++ public/js/admin_dashboard.js
@@ -3,14 +3,14 @@
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");
 
-function sidebarOpen() {
+function openSidebar() {
     if (!sidebarOpen) {
         sidebar.classList.add("sidebar-responsive");
         sidebarOpen = true;
     }
 }
 
-function sidebarClose() {
+function closeSidebar() {
     if (sidebarOpen) {
         sidebar.classList.remove("sidebar-responsive");
         sidebarOpen = false;
