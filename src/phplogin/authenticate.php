fd385bfae31971f51c6f8ec26527a9e0fab971f6
Marek Szulc <235991@edu.p.lodz.pl>
2022-11-22 11:04:48 +0100

    Add account and password checker
    TODO verify everything is ecrypted later on

--- src/phplogin/authenticate.php
+++ src/phplogin/authenticate.php
@@ -10,12 +10,12 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// ignores data not submitted via form
+/* ignores data not submitted via form */
 if (!isset($_POST['username'], $_POST['password'])) {
 	exit('Please fill both the username and password fields!');
 }
 
-// use $stmt to prevent SQL injection
+/* use $stmt to prevent SQL injection */
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -23,6 +23,36 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 
 	$stmt->close();
 }
+
+/* check if acc exists */
+if ($stmt->num_rows > 0) {
+	$stmt->bind_result($id, $password);
+	$stmt->fetch();
+	/* acc exists
+	   verify pass */
+	/* TODO: make sure everything about encryption works
+	   only passwords generated with password_hash() will work
+	*/
+	if (password_verify($_POST['password'], $password)) {
+		/* create sessions */
+
+		/* session_regenerate_id() also helps prevent session hijacking
+		it regenerates the user's session ID that is stored on the server
+		   and as a cookie in the browser */
+		session_regenerate_id();
+		$_SESSION['loggedin'] = TRUE;
+		$_SESSION['name'] = $_POST['username'];
+		$_SESSION['id'] = $id;
+		echo 'Welcome ' . $_SESSION['name'] . '!';
+	} else {
+		echo 'Incorrect username and/or password';
+	}
+} else {
+	echo 'Incorrect username and/or password';
+}
+
+
+
 
 
 ?>
