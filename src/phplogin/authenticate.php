c5582e5266b633f89de88fffb1e42fc5eafb53e1
Marek Szulc <235991@edu.p.lodz.pl>
2022-11-22 10:31:28 +0100

    add basic authenticate.php

--- src/phplogin/authenticate.php
+++ src/phplogin/authenticate.php
@@ -1,7 +1,28 @@
 <?php
 session_start();
-// Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// ignores data not submitted via form
+if (!isset($_POST['username'], $_POST['password'])) {
+	exit('Please fill both the username and password fields!');
+}
+
+// use $stmt to prevent SQL injection
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	$stmt->store_result();
+
+	$stmt->close();
+}
+
+
+?>
