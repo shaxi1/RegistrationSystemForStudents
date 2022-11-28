ae52d8a23b04471204a91384ec7d6f8fd637ec1f
Marek Szulc <235991@edu.p.lodz.pl>
2022-11-28 21:16:08 +0100

    fix basic login script
    fixed the order of code in authenticate.php which
    resulted in basic working login

--- public/phplogin/authenticate.php
+++ public/phplogin/authenticate.php
@@ -21,38 +21,33 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 	$stmt->execute();
 	$stmt->store_result();
 
-	$stmt->close();
-}
+	/* check if acc exists */
+	if ($stmt->num_rows > 0) {
+		$stmt->bind_result($id, $password);
+		$stmt->fetch();
+		/* acc exists
+		verify pass */
+		/* TODO: make sure everything about encryption works
+		only passwords generated with password_hash() will work
+		*/
+		if (password_verify($_POST['password'], $password)) {
+			/* create sessions */
 
-/* check if acc exists */
-if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-	/* acc exists
-	   verify pass */
-	/* TODO: make sure everything about encryption works
-	   only passwords generated with password_hash() will work
-	*/
-	if (password_verify($_POST['password'], $password)) {
-		/* create sessions */
-
-		/* session_regenerate_id() also helps prevent session hijacking
-		it regenerates the user's session ID that is stored on the server
-		   and as a cookie in the browser */
-		session_regenerate_id();
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
-		echo 'Welcome ' . $_SESSION['name'] . '!';
+			/* session_regenerate_id() also helps prevent session hijacking
+			it regenerates the user's session ID that is stored on the server
+			and as a cookie in the browser */
+			session_regenerate_id();
+			$_SESSION['loggedin'] = TRUE;
+			$_SESSION['name'] = $_POST['username'];
+			$_SESSION['id'] = $id;
+			echo 'Welcome ' . $_SESSION['name'] . '!';
+		} else {
+			echo 'Incorrect username and/or password';
+		}
 	} else {
 		echo 'Incorrect username and/or password';
 	}
-} else {
-	echo 'Incorrect username and/or password';
+
+	$stmt->close();
 }
-
-
-
-
-
 ?>
