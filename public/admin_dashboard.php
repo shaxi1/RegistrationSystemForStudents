4bd3b1b6583011dc651d0e0fc05c5ad806e1095a
Marek Szulc <235991@edu.p.lodz.pl>
2022-11-28 23:11:42 +0100

    remove unecessary tab from admin_dashboard.php

--- public/admin_dashboard.php
+++ public/admin_dashboard.php
@@ -105,7 +105,7 @@
         </main>
 
     </div>
-    
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="js/menu_dashboard.js"></script>
     <script src="js/logout.js"></script>
