8a5bca182cadafaeeae6f458865f590440adb85f
Marek Szulc <235991@edu.p.lodz.pl>
2022-12-05 16:50:52 +0100

    move profile.css to css folder

--- public/phplogin/profile.php
+++ public/phplogin/profile.php
@@ -7,7 +7,7 @@ session_start();
 	<head>
 		<meta charset="utf-8">
 		<title>Strona Profilowa</title>
-		<link href="profile.css" rel="stylesheet" type="text/css">
+		<link href="http://localhost/css/profile.css" rel="stylesheet" type="text/css">
 	</head>
 
 	<body class="loggedin">
