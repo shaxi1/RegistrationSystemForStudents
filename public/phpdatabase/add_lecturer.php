10c27d298c448b2b23be946e5d0d1afd2350ce66
Marek Szulc <235991@edu.p.lodz.pl>
2022-12-19 20:37:14 +0100

    fix spaces to tabs

--- public/phpdatabase/add_lecturer.php
+++ public/phpdatabase/add_lecturer.php
@@ -7,12 +7,12 @@ $ADMIN = 'admin';
 
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 /* ignores data not submitted via form */
 if (!isset($_POST['name'], $_POST['surname'], $_POST['degree'])) {
-    exit('Please fill all fields!');
+	exit('Please fill all fields!');
 }
 
 $_POST = array_map("trim", $_POST);
@@ -21,10 +21,10 @@ $_POST = array_map("htmlspecialchars", $_POST);
 /* use statements to prevent SQL injection
    statement for lecturer table */
 if ($stmt_lecturer = $conn->prepare("INSERT INTO lecturer (name, surname, degree) VALUES (?, ?, ?)")) {
-    $stmt_lecturer->bind_param("sss", $_POST['name'], $_POST['surname'], $_POST['degree']);
-    $stmt_lecturer->execute();
+	$stmt_lecturer->bind_param("sss", $_POST['name'], $_POST['surname'], $_POST['degree']);
+	$stmt_lecturer->execute();
 } else {
-    exit('Error inserting to lecturer table!');
+	exit('Error inserting to lecturer table!');
 }
 
 $stmt_lecturer->close();
